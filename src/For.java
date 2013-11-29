7adc44228b4afc4faa727f8f3bba2763cd6813c1
Jordan Aupetit <jaupetit@emi.u-bordeaux1.fr>
2013-11-29 16:57:14 +0100

    Debut de la gestion des For

--- src/For.java
+++ src/For.java
@@ -2,17 +2,32 @@ public class For extends Instruction {
 
     private Instruction affectation;
     private Expression condition;
-    private Expression increment;
+    private Instruction increment;
 
     private Instruction self;
 
-    public For(Instruction affectation, Expression condition, Expression increment, Instruction self) {
+    public For(Instruction affectation, Expression condition, Instruction increment, Instruction self) {
         this.affectation = affectation;
         this.condition = condition;
         this.increment = increment;
         this.self = self;
     }
 
-    public void genererCode(Label suivant) { }
+    public void genererCode(Label suivant) { 
+
+
+        Label avantFor = new Label();
+        Label selfLabel = new Label();
+
+        affectation.genererCode(avantFor);
+        System.out.println(avantFor + ":");
+        condition.genererJumpCode(selfLabel, suivant);
+
+        System.out.println(selfLabel + ":");
+        self.genererCode(avantFor);
+        increment.genererCode(avantFor);
+        System.out.println("goto "+ avantFor);
+
+    }
 }
 
