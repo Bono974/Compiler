6078e89bbf9f94227bcfd5a9808944e460dcbf09
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2013-12-02 23:14:22 +0100

    Début des procédures

---
+++ src/ProcedureAppel.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class ProcedureAppel extends Instruction {
+
+    private String nom;
+    private LinkedList<Expression> listeExpression;
+
+    public ProcedureAppel(String nom, LinkedList<Expression> list) {
+        this.nom = nom;
+        this.listeExpression = list;
+    }
+
+    public void genererCode(Label suivant) {
+        for (Expression e: this.listeExpression) {
+            Identifiant resTmp = e.genererCode();
+            System.out.println("param "+ resTmp);
+        }
+        System.out.println("call "+ this.nom /*+
+                           "\n"+ suivant*/);
+    }
+}
+
