d95017641db33d33ac3eed69f4610f0335c29a32
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2013-12-07 10:15:48 +0100

    Déclaration d'une liste de variable fonctionnel : int a, b, c;

--- src/instruction/Declaration.java
+++ src/instruction/Declaration.java
@@ -1,22 +1,26 @@
 package instruction;
 
+import java.util.List;
 import expression.Expression;
 import tac.Label;
 
 public class Declaration extends Instruction {
 
-    private Expression variable;
+    private List<Expression> variable;
     //Son type
     //private EnumType type;
 
-    public Declaration(/*EnumType type,*/ Expression variable) {
+    public Declaration(/*EnumType type,*/ List<Expression> variable) {
         //this.type = type;
         this.variable = variable;
     }
 
     public void genererCode(Label suivant) {
-        //ADD dans table des variables
-        //tableVariable.add(this.type, this.variable);
+        //for (Expression id: this.variable){
+        //    if (id.type == EnumType.VARIABLE)
+        //        ADD dans table des variables
+        //        tableVariable.add(this.type, this.variable);
+        //}
     }
 }
 
